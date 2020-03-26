29a192bf175f3951061e78f4bb4c215c50942d3d
Kotie Heim <kotieheim@gmail.com>
2020-03-26 10:05:40 -0600

    changed code

--- app.js
+++ app.js
@@ -18,23 +18,29 @@ odd number is previous number and next number ADDED together
 for example, array of [3, 6, 9] for the number 6 you would mult
 3 * 9 and replace 6 in the array with that value, 27. for undefined
 treat them as 1's
+
+if at point 0 and even take next number * 1, if odd take next number + 1
+if at last point and even take prev number * 1, if odd take prev number + 1
 */
 
 function evenTheOdds(arr) {
   let result = [];
   for (let i = 0; i < arr.length; i++) {
-    index = arr.indexOf(arr[i]);
-    console.log(arr[i], "at index", index);
+    console.log(arr[i], "at index", i);
     if (arr[i] % 2 === 0) {
-      if (i === 0 || i === arr.length - 1) {
-        result.push(arr[i] * 1);
+      if (i === 0) {
+        result.push(arr[i + 1]);
+      } else if (i === arr.length - 1) {
+        result.push(arr[i - 1]);
       }
-      result.push(arr[index - 1] * arr[index + 1]);
+      result.push(arr[i - 1] * arr[i + 1]);
     } else if (arr[i] % 2 !== 0) {
-      if (i === 0 || i === arr.length - 1) {
-        result.push(arr[i] + 1);
+      if (i === 0) {
+        result.push(arr[i + 1] + 1);
+      } else if (i === arr.length - 1) {
+        result.push(arr[i - 1] + 1);
       }
-      result.push(arr[index - 1] + arr[index + 1]);
+      result.push(arr[i - 1] + arr[i + 1]);
     }
   }
   const endArr = result.filter(value => {
@@ -42,6 +48,7 @@ function evenTheOdds(arr) {
   });
   return endArr;
 }
-// console.log(evenTheOdds([1, 4, 5, 6, 21, 34, 3, 27, 32]));
-// expected output = [2, 5, 10, 105, 40, 63, 61, 35, 33];
-console.log(evenTheOdds([3, 6, 9]));
+console.log(evenTheOdds([1, 4, 5, 6, 21, 34, 3, 27, 32]));
+// expect output = [5, 5, 10, 105, 40, 63, 61, 35, 32];
+console.log(evenTheOdds([3, 6, 9, 12]));
+// expect output = [7, 27, 18, 9]
